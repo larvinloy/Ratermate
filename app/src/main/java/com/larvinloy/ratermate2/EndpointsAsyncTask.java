2e6b9e31eb0adf23323250fb413ac4937b801117
rmit-s3565542-andrew-finlayson <s3565542@student.rmit.edu.au>
2016-10-17 23:03:24 +1100

    Forgot Session ID Field

--- app/src/main/java/com/larvinloy/ratermate2/EndpointsAsyncTask.java
+++ app/src/main/java/com/larvinloy/ratermate2/EndpointsAsyncTask.java
@@ -37,20 +37,23 @@ class EndpointsAsyncTask extends AsyncTask<Void, Void, Session> {
     @Override
     protected Session doInBackground(Void... params) {
         if(myApiService == null) { // Only do this once
-            SessionApi.Builder builder = new
-                    SessionApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-// options for running against local devappserver
-// — 10.0.2.2 is localhost’s IP address in Android emulator
-// — turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                @Override
-                public void initialize(AbstractGoogleClientRequest<?>    abstractGoogleClientRequest) throws IOException {
-                    abstractGoogleClientRequest.setDisableGZipContent(true);
-                }
-            });
+//            SessionApi.Builder builder = new
+//                    SessionApi.Builder(AndroidHttp.newCompatibleTransport(),
+//                    new AndroidJsonFactory(), null)
+//// options for running against local devappserver
+//// — 10.0.2.2 is localhost’s IP address in Android emulator
+//// — turn off compression when running against local devappserver
+//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+//            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                @Override
+//                public void initialize(AbstractGoogleClientRequest<?>    abstractGoogleClientRequest) throws IOException {
+//                    abstractGoogleClientRequest.setDisableGZipContent(true);
+//                }
+//            });
 // end options for devappserver
+
+            SessionApi.Builder builder = new SessionApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(),null)
+                    .setRootUrl("https://ratermate.appspot.com/_ah/api/");
 
             myApiService = builder.build();
         }
@@ -64,7 +67,6 @@ class EndpointsAsyncTask extends AsyncTask<Void, Void, Session> {
             test.setG("1111111111111");
             test.setN("3333333333333");
             test.setModLength(1024);
-            test.setPasskey("password");
 
             test.setCategories(new ArrayList<String>(){{
                 add("Apples");
