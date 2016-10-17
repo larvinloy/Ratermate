2e90a2d2b741628ac32793b22744e3800c63348b
Loy Larvin <s3562412@student.rmit.edu.au>
2016-10-17 23:22:42 +1100

    Vote endpoint default

---
+++ api/src/main/java/com/example/larvinloy/myapplication/backend/VoteEndpoint.java
@@ -0,0 +1,159 @@
+package com.example.larvinloy.myapplication.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.response.CollectionResponse;
+import com.google.api.server.spi.response.NotFoundException;
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.datastore.QueryResultIterator;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.cmd.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.annotation.Nullable;
+import javax.inject.Named;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+/**
+ * WARNING: This generated code is intended as a sample or starting point for using a
+ * Google Cloud Endpoints RESTful API with an Objectify entity. It provides no data access
+ * restrictions and no data validation.
+ * <p>
+ * DO NOT deploy this code unchanged as part of a real application to real users.
+ */
+@Api(
+        name = "voteApi",
+        version = "v1",
+        resource = "vote",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.larvinloy.example.com",
+                ownerName = "backend.myapplication.larvinloy.example.com",
+                packagePath = ""
+        )
+)
+public class VoteEndpoint {
+
+    private static final Logger logger = Logger.getLogger(VoteEndpoint.class.getName());
+
+    private static final int DEFAULT_LIST_LIMIT = 20;
+
+    static {
+        // Typically you would register this inside an OfyServive wrapper. See: https://code.google.com/p/objectify-appengine/wiki/BestPractices
+        ObjectifyService.register(Vote.class);
+    }
+
+    /**
+     * Returns the {@link Vote} with the corresponding ID.
+     *
+     * @param voteId the ID of the entity to be retrieved
+     * @return the entity with the corresponding ID
+     * @throws NotFoundException if there is no {@code Vote} with the provided ID.
+     */
+    @ApiMethod(
+            name = "get",
+            path = "vote/{voteId}",
+            httpMethod = ApiMethod.HttpMethod.GET)
+    public Vote get(@Named("voteId") Long voteId) throws NotFoundException {
+        logger.info("Getting Vote with ID: " + voteId);
+        Vote vote = ofy().load().type(Vote.class).id(voteId).now();
+        if (vote == null) {
+            throw new NotFoundException("Could not find Vote with ID: " + voteId);
+        }
+        return vote;
+    }
+
+    /**
+     * Inserts a new {@code Vote}.
+     */
+    @ApiMethod(
+            name = "insert",
+            path = "vote",
+            httpMethod = ApiMethod.HttpMethod.POST)
+    public Vote insert(Vote vote) {
+        // Typically in a RESTful API a POST does not have a known ID (assuming the ID is used in the resource path).
+        // You should validate that vote.voteId has not been set. If the ID type is not supported by the
+        // Objectify ID generator, e.g. long or String, then you should generate the unique ID yourself prior to saving.
+        //
+        // If your client provides the ID then you should probably use PUT instead.
+        ofy().save().entity(vote).now();
+        logger.info("Created Vote with ID: " + vote.getVoteId());
+
+        return ofy().load().entity(vote).now();
+    }
+
+    /**
+     * Updates an existing {@code Vote}.
+     *
+     * @param voteId the ID of the entity to be updated
+     * @param vote   the desired state of the entity
+     * @return the updated version of the entity
+     * @throws NotFoundException if the {@code voteId} does not correspond to an existing
+     *                           {@code Vote}
+     */
+    @ApiMethod(
+            name = "update",
+            path = "vote/{voteId}",
+            httpMethod = ApiMethod.HttpMethod.PUT)
+    public Vote update(@Named("voteId") Long voteId, Vote vote) throws NotFoundException {
+        // TODO: You should validate your ID parameter against your resource's ID here.
+        checkExists(voteId);
+        ofy().save().entity(vote).now();
+        logger.info("Updated Vote: " + vote);
+        return ofy().load().entity(vote).now();
+    }
+
+    /**
+     * Deletes the specified {@code Vote}.
+     *
+     * @param voteId the ID of the entity to delete
+     * @throws NotFoundException if the {@code voteId} does not correspond to an existing
+     *                           {@code Vote}
+     */
+    @ApiMethod(
+            name = "remove",
+            path = "vote/{voteId}",
+            httpMethod = ApiMethod.HttpMethod.DELETE)
+    public void remove(@Named("voteId") Long voteId) throws NotFoundException {
+        checkExists(voteId);
+        ofy().delete().type(Vote.class).id(voteId).now();
+        logger.info("Deleted Vote with ID: " + voteId);
+    }
+
+    /**
+     * List all entities.
+     *
+     * @param cursor used for pagination to determine which page to return
+     * @param limit  the maximum number of entries to return
+     * @return a response that encapsulates the result list and the next page token/cursor
+     */
+    @ApiMethod(
+            name = "list",
+            path = "vote",
+            httpMethod = ApiMethod.HttpMethod.GET)
+    public CollectionResponse<Vote> list(@Nullable @Named("cursor") String cursor, @Nullable @Named("limit") Integer limit) {
+        limit = limit == null ? DEFAULT_LIST_LIMIT : limit;
+        Query<Vote> query = ofy().load().type(Vote.class).limit(limit);
+        if (cursor != null) {
+            query = query.startAt(Cursor.fromWebSafeString(cursor));
+        }
+        QueryResultIterator<Vote> queryIterator = query.iterator();
+        List<Vote> voteList = new ArrayList<Vote>(limit);
+        while (queryIterator.hasNext()) {
+            voteList.add(queryIterator.next());
+        }
+        return CollectionResponse.<Vote>builder().setItems(voteList).setNextPageToken(queryIterator.getCursor().toWebSafeString()).build();
+    }
+
+    private void checkExists(Long voteId) throws NotFoundException {
+        try {
+            ofy().load().type(Vote.class).id(voteId).safe();
+        } catch (com.googlecode.objectify.NotFoundException e) {
+            throw new NotFoundException("Could not find Vote with ID: " + voteId);
+        }
+    }
+}
