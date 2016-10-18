ebeac978490fc6451831d223b7c57c33a38782c2
Loy Larvin <s3562412@student.rmit.edu.au>
2016-10-18 23:10:24 +1100

    Refactoring

--- app/src/main/java/com/larvinloy/ratermate2/GetSessionEndpointsAsyncTask.java
+++ app/src/main/java/com/larvinloy/ratermate2/GetSessionEndpointsAsyncTask.java
@@ -56,9 +56,21 @@ class GetSessionEndpointsAsyncTask extends AsyncTask<Void, Void, Session>
         sessionID = MainActivity.getSessionID();
 
         if(myApiService == null) { // Only do this once
-
-            SessionApi.Builder builder = new SessionApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(),null)
-                    .setRootUrl("https://ratermate.appspot.com/_ah/api/");
+            SessionApi.Builder builder = new
+                    SessionApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+// options for running against local devappserver
+// — 10.0.2.2 is localhost’s IP address in Android emulator
+// — turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?>    abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+//            SessionApi.Builder builder = new SessionApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(),null)
+//                    .setRootUrl("https://ratermate.appspot.com/_ah/api/");
 
             myApiService = builder.build();
         }
